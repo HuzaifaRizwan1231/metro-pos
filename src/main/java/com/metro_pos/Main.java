94ed42a40e2cafae773fa9941b2cca36372061de
Huzaifa Rizwan <huzaifa.rizwan1231@gmail.com>
2024-10-30 13:50:56 +0500

    test push from vscode dev

--- src/main/java/com/metro_pos/Main.java
+++ src/main/java/com/metro_pos/Main.java
@@ -2,6 +2,6 @@ package main.java.com.metro_pos;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello not world!");
     }
 }
