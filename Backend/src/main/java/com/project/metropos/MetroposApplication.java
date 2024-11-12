5198b326a591cbe5a75aff4f2c92a2867cd8a4f1
Huzaifa Rizwan <huzaifa.rizwan1231@gmail.com>
2024-11-13 01:40:49 +0500

    basic backend structre setup

---
+++ Backend/src/main/java/com/project/metropos/MetroposApplication.java
@@ -0,0 +1,13 @@
+package com.project.metropos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetroposApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetroposApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/project/metropos/MetroposApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.metropos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetroposApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
